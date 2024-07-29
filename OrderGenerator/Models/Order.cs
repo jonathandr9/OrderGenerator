3e1a3f3abe6623f84ffc7944da19b2fb2c74fcb0
jonathandr9 <jonathandias96@gmail.com>
2024-07-29 00:09:11 -0300

    Add project files.

---
+++ OrderGenerator/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace OrderGenerator.Models
+{
+    public class Order
+    {
+        public AssetsList Asset { get; set; }
+        public string Side { get; set; }    
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public enum AssetsList
+    {
+        PETR4,
+        VALE3,
+        VIIA4
+    }
+}
